6da4424c9f092d3f8259373b2d7806be584c2ece
Roch Bajracharya <rochbajracharya@Rochs-MacBook-Pro.local>
2023-10-05 18:40:47 -0400

    background inage and social footer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import './App.scss';
+import FacebookSVG from "./svg/facebook";
+import InstgramSVG from "./svg/instagram";
+import PinterestSVG from "./svg/pinterest";
 
 function App() {
   return (
-   <div>Hello</div>
+    <div className="wrapper">
+      <div>
+        <h2>We're Launching Soon</h2>
+      </div>
+      <div>Timer</div>
+      <footer>
+        <a href="#">
+          <FacebookSVG/>
+        </a>
+        <a href="#">
+          <InstgramSVG/>
+        </a>
+        <a href="#">
+         <PinterestSVG/>
+        </a>
+      </footer>
+    </div>
   );
 }
 
